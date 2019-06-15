bddde29b8bee7895e13bba3e9cfabfc172e9e7fb
Gareth Jones <jones258@gmail.com>
2019-06-15 23:58:03 +1200

    Ensure that lambda responses have a string body

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+/**
+ * Ensures that the given `Response` has a body of type `string`.
+ *
+ * @param {ApiGatewayResponse} response
+ *
+ * @return {ApiGatewayResponse}
+ */
+const ensureStringBody = response => ({
+  ...response,
+  body: typeof response.body === 'object'
+    ? JSON.stringify(response.body)
+    : response.body
+});
+
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-exports.handler = require('./src/index').handler;
+exports.handler = event => require('./src/index').handler(event).then(ensureStringBody);
