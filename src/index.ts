0ac2492ef194ba75b61590f1919b3154fb86008f
Gareth Jones <jones258@gmail.com>
2019-06-16 00:00:39 +1200

    Type `request` parameter in `index`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export const handler = async (request: object) => {
+type StringMap = Record<string, string>;
+
+export interface ApiGatewayRequest {
+  resource: string;
+  path: string;
+  httpMethod: string;
+  headers: StringMap;
+  multiValueHeaders: Record<string, string[]>;
+  queryStringParameters: StringMap;
+  pathParameters: StringMap;
+  stageVariables: StringMap;
+  requestContext: object;
+  body: string;
+  isBase64Encoded: boolean;
+}
+
+export const handler = async (request: ApiGatewayRequest) => {
 
 };
