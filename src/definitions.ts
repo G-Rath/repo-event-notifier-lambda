8da8f9d05b9ad153b8b907374f64e30acf0c5783
Gareth Jones <jones258@gmail.com>
2019-06-21 18:49:00 +1200

    Adjust `StringMap` definition to be a map of `string | undefined`
    This is more accurate, since the properties might not be defined.

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,4 @@
-export type StringMap = Record<string, string>;
+export type StringMap = Record<string, string | undefined>;
 
 export interface ApiGatewayRequest {
   resource: string;
@@ -37,3 +37,10 @@ export interface ApiGatewayResponse {
   statusCode: number;
   body: string | object;
 }
+
+// body:
+// '{"token":"93fTY14e1V48e47aL87F6Kta","team_id":"T025T9LHG","api_app_id":"AH9FX64G7","event":{"type":"message","subtype":"message_changed","hidden":true,"message":{"client_msg_id":"0a6ba8de-fba7-446d-aaec-a320d6eb2954","type":"message","text":"review
+// please:
+// <https:\\/\\/bitbucket.org\\/preferizi\\/theraces-react\\/pull-requests\\/70\\/add-code-for-google-tags-manager\\/diff>","user":"UEJSTE4JU","edited":{"user":"UEJSTE4JU","ts":"1560308802.000000"},"ts":"1560304286.009100"},"channel":"CDMBDU06S","previous_message":{"client_msg_id":"0a6ba8de-fba7-446d-aaec-a320d6eb2954","type":"message","text":"review
+// please:
+// <https:\\/\\/bitbucket.org\\/preferizi\\/theraces-react\\/pull-requests\\/70\\/add-code-for-google-tags-manager\\/diff>","user":"UEJSTE4JU","ts":"1560304286.009100"},"event_ts":"1560308802.010400","ts":"1560308802.010400","channel_type":"channel"},"type":"event_callback","event_id":"EvKFT2DDT6","event_time":1560308802,"authed_users":["UEJSTE4JU"]}',
