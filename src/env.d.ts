8c284a84e24c2920236c2ec90cd9e7998cecfc68
Gareth Jones <jones258@gmail.com>
2019-06-21 18:16:33 +1200

    Define `SLACK_SIGNING_SECRET` environment variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-
+    /**
+     * The signing secret for slack.
+     *
+     * Used to validate requests claiming to be from slack.
+     */
+    SLACK_SIGNING_SECRET: string;
   }
 }
