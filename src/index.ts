194d7eb0a4a42d1ef8ba5cda30bc595b36cb3162
Gareth Jones <jones258@gmail.com>
2019-07-04 19:09:28 +1200

    Implement usage of `isRequestFromSlack` & `handleSlackRequest` in `handler` fn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { ApiGatewayRequest, ApiGatewayResponse } from '@src/definitions';
+import { handleSlackRequest } from '@src/handlers';
+import { isRequestFromSlack } from '@src/utils';
 import * as HttpStatus from 'http-status-codes';
 
 export const handler = async (request: ApiGatewayRequest): Promise<ApiGatewayResponse> => {
+  if (isRequestFromSlack(request)) {
+    return handleSlackRequest(request);
+  }
+
   return {
     statusCode: HttpStatus.NOT_FOUND,
     body: {}
--- test/src/index.spec.ts
+++ test/src/index.spec.ts
@@ -1,17 +1,43 @@
+import { handleSlackRequest } from '@src/handlers';
 import { handler } from '@src/index';
+import { isRequestFromSlack } from '@src/utils';
 import { buildApiGatewayRequest } from '@test/factories';
 import * as HttpStatus from 'http-status-codes';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('@src/utils/isRequestFromSlack');
+jest.mock('@src/handlers/handleSlackRequest');
+
+const mockedIsRequestFromSlack = mocked(isRequestFromSlack);
+const mockedHandleSlackRequest = mocked(handleSlackRequest);
 
 describe('index', () => {
-  it('responds w/ a "statusCode" of 404 (NOT_FOUND)', async () => {
-    const response = await handler(buildApiGatewayRequest());
+  describe('when the request is from slack', () => {
+    beforeEach(() => mockedIsRequestFromSlack.mockReturnValue(true));
 
-    expect(response.statusCode).toStrictEqual(HttpStatus.NOT_FOUND);
+    it('delegates to handleSlackRequest', async () => {
+      const request = buildApiGatewayRequest();
+
+      await handler(request);
+
+      expect(mockedIsRequestFromSlack).toHaveBeenCalledWith(request);
+      expect(mockedHandleSlackRequest).toHaveBeenCalledWith(request);
+    });
   });
 
-  it('responds /w an empty "body"', async () => {
-    const response = await handler(buildApiGatewayRequest());
+  describe('when the request is not from slack', () => {
+    beforeEach(() => mockedIsRequestFromSlack.mockReturnValue(false));
 
-    expect(response.body).toStrictEqual({});
+    it('responds w/ a "statusCode" of 404 (NOT_FOUND)', async () => {
+      const response = await handler(buildApiGatewayRequest());
+
+      expect(response.statusCode).toStrictEqual(HttpStatus.NOT_FOUND);
+    });
+
+    it('responds w/ an empty "body"', async () => {
+      const response = await handler(buildApiGatewayRequest());
+
+      expect(response.body).toStrictEqual({});
+    });
   });
 });
